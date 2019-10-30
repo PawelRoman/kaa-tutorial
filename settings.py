f95598dac4f55abcfb22a052216c6070eb0911cd
Pawel Roman <romapawel@gmail.com>
2019-10-30 20:44:54 +0100

    end of part 3 code

--- settings.py
+++ settings.py
@@ -1,8 +1,4 @@
 # Let's use full HD as a base resolution for our game!
-# VIEWPORT_WIDTH = 1920
-# VIEWPORT_HEIGHT = 1080
-
-
-VIEWPORT_WIDTH = 1000
-VIEWPORT_HEIGHT = 800
+VIEWPORT_WIDTH = 1920
+VIEWPORT_HEIGHT = 1080
 
