93c8c34582f3bb430494be797dfb685b80aa9eaf
Pawel Roman <romapawel@gmail.com>
2020-02-25 21:49:25 +0100

    code reformatting

--- scenes/title_screen.py
+++ scenes/title_screen.py
@@ -17,16 +17,15 @@ class TitleScreenScene(Scene):
     def __init__(self):
         super().__init__()
         self.root.add_child(Node(sprite=registry.global_controllers.assets_controller.title_screen_background_img,
-                                 z_index=0, position=Vector(0,0), origin_alignment=Alignment.top_left))
+                                 z_index=0, position=Vector(0, 0), origin_alignment=Alignment.top_left))
         self.root.add_child(TextNode(font=registry.global_controllers.assets_controller.font_2, font_size=30,
-                                     position=Vector(settings.VIEWPORT_WIDTH/2, 500), text="Click to start the game",
+                                     position=Vector(settings.VIEWPORT_WIDTH / 2, 500), text="Click to start the game",
                                      z_index=1, origin_alignment=Alignment.center))
         self.exit_label = TextNode(font=registry.global_controllers.assets_controller.font_2, font_size=30,
-                                     position=Vector(settings.VIEWPORT_WIDTH/2, 550), text="Press ESC to exit",
-                                     z_index=1, origin_alignment=Alignment.center)
+                                   position=Vector(settings.VIEWPORT_WIDTH / 2, 550), text="Press ESC to exit",
+                                   z_index=1, origin_alignment=Alignment.center)
         self.root.add_child(self.exit_label)
         self.transitions_fun_stuff()
-
 
     def start_new_game(self):
         registry.scenes.gameplay_scene = GameplayScene()
@@ -36,21 +35,23 @@ class TitleScreenScene(Scene):
         registry.global_controllers.assets_controller.explosion_sound.play()
 
     def transitions_fun_stuff(self):
-        rotate_transition = NodeRotationTransition(2*math.pi, duration=1000) # rotate 180 degrees (2*pi radians)
-        scale_transition = NodeScaleTransition(Vector(2, 2), duration=1000) # enlarge twice
-        color_transition = NodeColorTransition(Color(1, 0, 0, 1), duration=1000) # change color to red
+        rotate_transition = NodeRotationTransition(2 * math.pi, duration=1000)  # rotate 180 degrees (2*pi radians)
+        scale_transition = NodeScaleTransition(Vector(2, 2), duration=1000)  # enlarge twice
+        color_transition = NodeColorTransition(Color(1, 0, 0, 1), duration=1000)  # change color to red
 
         move_transition1 = NodePositionTransition(Vector(-200, 0), duration=1000,
-                                           advance_method=AttributeTransitionMethod.add)
+                                                  advance_method=AttributeTransitionMethod.add)
         move_transition2 = NodePositionTransition(Vector(200, 200), duration=1000,
-                                           advance_method=AttributeTransitionMethod.add)
+                                                  advance_method=AttributeTransitionMethod.add)
         move_transition3 = NodePositionTransition(Vector(200, -200), duration=1000,
-                                           advance_method=AttributeTransitionMethod.add)
+                                                  advance_method=AttributeTransitionMethod.add)
         move_transition4 = NodePositionTransition(Vector(-200, 0), duration=1000,
-                                           advance_method=AttributeTransitionMethod.add)
+                                                  advance_method=AttributeTransitionMethod.add)
 
-        move_sequence = NodeTransitionsSequence([move_transition1, move_transition2, move_transition3, move_transition4], loops=0)
-        paralel_sequence = NodeTransitionsParallel([rotate_transition, scale_transition, color_transition], back_and_forth=True, loops=0)
+        move_sequence = NodeTransitionsSequence(
+            [move_transition1, move_transition2, move_transition3, move_transition4], loops=0)
+        paralel_sequence = NodeTransitionsParallel([rotate_transition, scale_transition, color_transition],
+                                                   back_and_forth=True, loops=0)
 
         # run both the movement sequence and rotate+scale+color sequence in paralel
         self.exit_label.transition = NodeTransitionsParallel([
