f9d2cb3b8fb11e710510fd4080a86ef09fe12996
Pawel Roman <romapawel@gmail.com>
2020-04-13 19:35:57 +0200

    for the sake of formality...

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ from scenes.title_screen import TitleScreenScene
 import registry
 from controllers.assets_controller import AssetsController
 
-with Engine(virtual_resolution=Vector(settings.VIEWPORT_WIDTH, settings.VIEWPORT_HEIGHT)) as engine:
-    # initialize global controllers and keep them in the registry
-    registry.global_controllers.assets_controller = AssetsController()
-    # play music
-    registry.global_controllers.assets_controller.music_track_1.play()
-    # set window to fullscreen mode
-    engine.window.fullscreen = True
-    # initialize and run the scene
-    registry.scenes.gameplay_scene = GameplayScene()
-    registry.scenes.title_screen_scene = TitleScreenScene()
-    registry.scenes.pause_scene = PauseScene()
-    engine.run(registry.scenes.title_screen_scene)
+if __name__ == "__main__":
+
+    with Engine(virtual_resolution=Vector(settings.VIEWPORT_WIDTH, settings.VIEWPORT_HEIGHT)) as engine:
+        # initialize global controllers and keep them in the registry
+        registry.global_controllers.assets_controller = AssetsController()
+        # play music
+        registry.global_controllers.assets_controller.music_track_1.play()
+        # set window to fullscreen mode
+        engine.window.fullscreen = True
+        # initialize and run the scene
+        registry.scenes.gameplay_scene = GameplayScene()
+        registry.scenes.title_screen_scene = TitleScreenScene()
+        registry.scenes.pause_scene = PauseScene()
+        engine.run(registry.scenes.title_screen_scene)
